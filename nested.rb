9fbbca9fbaf2ce2d98cac355cef3c08a317027f6
Christina Gonzalez <cmgamway@gmail.com>
2019-08-31 08:18:12 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -117,7 +117,8 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+programmer_hash.each do |value|
 programmer_hash[:dennis_ritchie][:languages][3] = "Assembly"
-programmer_hash[:alan_kay][:languages].include?("Assembly")
+programmer_hash[:dennis_ritchie][:languages].include?("Assembly")
 programmer_hash
 end

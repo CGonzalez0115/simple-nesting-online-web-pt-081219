d022cae15d0c31ae9760db84d728c391ff888496
Christina Gonzalez <cmgamway@gmail.com>
2019-08-31 06:52:36 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -96,7 +96,7 @@ def changing_alan
      }
 
      alans_new_info = "GUI"
-programmer_hash = programmer_hash.replace("Object Orientation", "GUI")
+programmer_hash.replace[:alan_kay][:known_for] = "GUI"
 end
 
 def adding_to_dennis

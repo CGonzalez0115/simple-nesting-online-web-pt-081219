6235df7b6b0417973b440659020b7905594336d1
Christina Gonzalez <cmgamway@gmail.com>
2019-08-31 05:30:15 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -74,7 +74,8 @@ def adding_matz
           :languages => ["C"]
         }
     }
-    programmer_hash.store(:yukihiro_matsumoto => :known_for, "Ruby")
+    programmer_hash.store(:yukihiro_matsumoto => {})
+    => {:known_for=>"Ruby", :languages=>["LISP", "C"]}
     programmer_hash
   end
 end

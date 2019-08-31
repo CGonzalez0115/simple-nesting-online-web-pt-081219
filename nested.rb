88209eded55a050fe2641bb50d4f872237662dfd
Christina Gonzalez <cmgamway@gmail.com>
2019-08-31 05:26:04 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -74,12 +74,8 @@ def adding_matz
           :languages => ["C"]
         }
     }
-
-    programmer_hash = Hash.new do |h,k|
-      h[k] = Hash.new()
-      programmer_hash[:yukihiro_matsumoto]
-      programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
-      programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
+    programmer_hash.store(:yukihiro_matsumoto, (:known_for, "Ruby", :languages, ["LISP", "C"]))
+    programmer_hash
     end
   end
 

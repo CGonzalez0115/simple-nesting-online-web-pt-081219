9b1bf862d282a5ceb6dbcd5ba837324a3914bc45
Christina Gonzalez <cmgamway@gmail.com>
2019-08-31 07:16:19 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -118,5 +118,5 @@ def adding_to_dennis
         }
      }
   programmer_hash[:dennis_ritchie][:languages] << "Assembly"
-  programmer_hash[:dennis_ritchie][:languages].join(",").split(",")
+  programmer_hash[:dennis_ritchie][:languages].to_i.join(",").split(",")
 end
